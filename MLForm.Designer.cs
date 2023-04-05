554c8260eb39c8c63dc0ea89c864e354eac1c57b
Nicegomting <ggoo.kang@gmail.com>
2023-04-05 09:10:29 +0900

    초기 커밋.

---
+++ MLForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MLNET
+{
+    partial class MLForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.lblPreview = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.Location = new System.Drawing.Point(550, 15);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(42, 26);
+            this.btnFileSelect.TabIndex = 0;
+            this.btnFileSelect.Text = "...";
+            this.btnFileSelect.UseVisualStyleBackColor = true;
+            this.btnFileSelect.Click += new System.EventHandler(this.btnFileSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이미지";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(59, 18);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(485, 21);
+            this.txtPath.TabIndex = 2;
+            // 
+            // picPreview
+            // 
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Location = new System.Drawing.Point(12, 18);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(200, 200);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPreview.TabIndex = 3;
+            this.picPreview.TabStop = false;
+            // 
+            // lblPreview
+            // 
+            this.lblPreview.AutoSize = true;
+            this.lblPreview.Location = new System.Drawing.Point(76, 221);
+            this.lblPreview.Name = "lblPreview";
+            this.lblPreview.Size = new System.Drawing.Size(73, 12);
+            this.lblPreview.TabIndex = 4;
+            this.lblPreview.Text = "* 미리보기 *";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.lblResult);
+            this.groupBox.Controls.Add(this.picPreview);
+            this.groupBox.Controls.Add(this.lblPreview);
+            this.groupBox.Location = new System.Drawing.Point(12, 45);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(580, 241);
+            this.groupBox.TabIndex = 5;
+            this.groupBox.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(330, 105);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 5;
+            // 
+            // MLForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(604, 295);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFileSelect);
+            this.Controls.Add(this.groupBox);
+            this.Name = "MLForm";
+            this.Text = "MLForm";
+            this.Load += new System.EventHandler(this.MLForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Label lblPreview;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ MLForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLNET
+{
+    public partial class MLForm : Form
+    {
+        public MLForm()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        private void MLForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnFileSelect_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image File|*.jpg;*.jpeg;*.png;*.gif;";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = openFileDialog.FileName;
+                    picPreview.ImageLocation = openFileDialog.FileName;
+
+                    var result = IsCATorDOG(txtPath.Text);
+                    if(result.Item1 is null)
+                        lblResult.Text = $"알 수 없는 유형입니다.";
+                    else if(result.Item1 == true)
+                        lblResult.Text = $"이것은 {result.Item2*100.00:F2}% 떼껄룩 입니다.";
+                    else
+                        lblResult.Text = $"이것은 {result.Item2*100.00:F2}% 댕댕이 입니다.";
+                }
+            }
+        }
+
+        private Tuple<bool?, float> IsCATorDOG(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+
+            var imageBytes = File.ReadAllBytes(path);
+            MLModel.ModelInput sampleData = new MLModel.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            var result = MLModel.Predict(sampleData);
+            float score = result.Score.Max();
+            bool? isCAT = result.PredictedLabel.StartsWith("cat");
+            return new Tuple<bool?, float>(isCAT, score);
+        }
+    }
+}
